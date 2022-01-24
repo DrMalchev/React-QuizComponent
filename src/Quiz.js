56cdde799bda7c272283e3db07ea611ce9d471f5
DrMalchev <delyan.malchev@eveliko.com>
2022-01-24 10:38:35 +0200

    Work upload for Step 2.

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
         return (
             <div>
                 <div className="QuizQuestion">
-                    aaaa
+                    {quizData.quiz_questions[0].instruction_text}
                 </div>
             </div>)
     }
