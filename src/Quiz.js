f95b21b57522017f5d48b939687ad67c6d20bb2f
DrMalchev <delyan.malchev@eveliko.com>
2022-01-25 16:25:22 +0200

    Work on stage 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,6 +13,7 @@ class Quiz extends Component {
     render() {
         return (
             <div>
+                <QuizEnd />
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>)
     }
