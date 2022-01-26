ac51268ca17f548236c4b736b2c87a11fd5fb539
DrMalchev <delyan.malchev@eveliko.com>
2022-01-26 16:03:42 +0200

    6.7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,7 +3,9 @@ import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
     handleClick(buttonText) {
-
+        if (this.props.buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler()
+        }
     }
 
     render() {
